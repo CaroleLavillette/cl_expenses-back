6005522aa8e1e422eaaea5335bc93920bc4e3cb5
CaroleLavillette <pitouDev@users.noreply.github.com>
2019-09-04 15:01:17 +0200

    mongodb url et port

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-/* mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/expenses-app", {useNewUrlParser: true}); */
-mongoose.connect(
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/expenses-app", {useNewUrlParser: true});
+/* mongoose.connect(
     "mongodb://localhost/expenses-app",{ useNewUrlParser: true }
-  );
+  ); */
 let port = process.env.PORT || 3007;
 
 const Expense = require("./models/expenses");
